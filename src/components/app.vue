9f67d4b118808f52a77fdb2dd67cac440b8add56
ZJY <YY996618@qq.com>
2020-09-04 16:21:44 +0800

    zujian

--- src/components/app.vue
+++ src/components/app.vue
@@ -1,94 +1,65 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+    <h1 v-bind:title="hello">{{ hello }}</h1>
+    <h2 v-if="seen">Essential Links</h2>
+    <h2 v-show="seen">{{text}}</h2>
+    <h2 :[titles]="pkh">动态绑定值</h2>
+    <form>
+      <input type="text" v-model="text">
+      <input type="text" v-model="num1">+
+      <input type="text" v-model="num2">
+      <input type="text" v-model="sum">
+      <input type="text" v-model="sumFn">
+    </form>
     <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
+      <li v-for="item in date" :key="item.id" @click="clickFn(item.id)">{{item.name}}</li>
     </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <div v-html="chtml"> </div>
+    <foote :footers="footerDta"></foote>
   </div>
 </template>
 
 <script>
+import foote from '@/core/foot'
 export default {
   name: 'HelloWorld',
+  components: { // 组件
+    foote
+  },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      hello: 'hello Work' + new Date().toLocaleString(),
+      seen: true,
+      text: '显示',
+      titles: 'title',
+      pkh: '动态绑定值',
+      num1: 0,
+      num2: 0,
+      sum: 0,
+      footerDta: '底部版权',
+      chtml: '<p>p<p>',
+      date: [
+        {id: 1, name: 'w文字'},
+        {id: 2, name: 'n文字'}
+      ]
+    }
+  },
+  watch: {
+    num1: function () {
+      this.sum = Number(this.num1) + Number(this.num2)
+    },
+    num2: function () {
+      this.sum = Number(this.num1) + Number(this.num2)
+    }
+  },
+  computed: {// // 计算属性的 getter
+    sumFn: function () {
+      return (Number(this.num1) + Number(this.num2))
+    }
+  },
+  methods: { // 方法
+    clickFn: function (e) {
+      this.hello = this.hello.split('').reverse().join('')
     }
   }
 }
---
+++ src/core/foot.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="footer">
+    <p> {{footers}}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'footerName',
+  props: {
+    footers: {
+      type: String,
+      default: '底部'
+    }
+  },
+  data () {
+    return {}
+  }
+}
+</script>
